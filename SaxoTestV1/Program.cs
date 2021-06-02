785c298da55740470a735fdc89bf3f5136fb5576
msenger1987 <msenger1987@gmail.com>
2021-06-03 04:25:19 +0530

    Add files via upload

---
+++ SaxoTestV1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Calculatorlibrary;
+using System.IO;
+using System.Reflection;
+
+namespace SaxoTestV1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Input command"); 
+        
+         string input = Console.ReadLine();
+         Calculations calc = new Calculations();
+         do
+         {
+          string[] command = calc.GetCommandParameters(input);
+          if (Convert.ToString(command[0]) != "count")
+            {
+                Console.WriteLine(calc.DoOperation(command));
+            }
+            else{
+                Console.WriteLine(calc.GetCommandCount(command));
+            }
+           input = Console.ReadLine();
+        }while(input != string.Empty);
+    }
+}
+}
